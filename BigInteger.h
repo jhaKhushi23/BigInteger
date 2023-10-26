aaa3a92d502eacb3a17d611809ed173c4f26a2f3
Khushi Jha <146522750+jhaKhushi23@users.noreply.github.com>
2023-10-26 22:00:54 +0530

    Update BigInteger.h

--- BigInteger.h
+++ BigInteger.h
@@ -14,19 +14,20 @@ struct BigInteger{
 
 struct BigInteger initialize(char *s);
 
-// main functions
-struct BigInteger add(struct BigInteger list1, struct BigInteger list2);    
-struct BigInteger sub(struct BigInteger list1, struct BigInteger list2);    
-struct BigInteger mul(struct BigInteger list1, struct BigInteger list2);
-struct BigInteger div1(struct BigInteger list1, struct BigInteger list2);
+struct BigInteger add(struct BigInteger num1, struct BigInteger num2);    
+struct BigInteger sub(struct BigInteger num1, struct BigInteger num2);    
+struct BigInteger mul(struct BigInteger num1, struct BigInteger num2);
+struct BigInteger div1(struct BigInteger num1, struct BigInteger num2);
+struct BigInteger mod(struct BigInteger num1,struct BigInteger num2);
 
-// helper functions
-void insertAtHead(struct BigInteger *list, int val);
-void insertAtTail(struct BigInteger *list, int val);
+void insertAtHead(struct BigInteger *num, int val);
+void insertAtTail(struct BigInteger *num, int val);
 
-int compareForMag(struct BigInteger list1, struct BigInteger list2);
-void removeZeroes(struct BigInteger *list);
+int compare(struct BigInteger num1, struct BigInteger num2);
+void trimZero(struct BigInteger *num);
+void freeBigInteger(struct BigInteger *num);
+int calculateLength(struct BigInteger *num);
 
-void display(struct BigInteger list);
+void display(struct BigInteger num);
 
 #endif
